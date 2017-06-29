8a7bf16875bb7def0879a5ad09fbddcfde080eeb
张山海 <zhangshanhai@users.noreply.github.com>
2017-06-30 00:31:59 +0800

    Update QRterminal.java

--- QRterminal.java
+++ QRterminal.java
@@ -2,11 +2,9 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
-import java.awt.image.BufferedImage;
 import java.util.Hashtable;
 
 public class QRterminal {
@@ -17,12 +15,12 @@ public class QRterminal {
 		int height = 40;
 		// 用于设置QR二维码参数
 		Hashtable<EncodeHintType, Object> qrParam = new Hashtable<EncodeHintType, Object>();
-		// 设置QR二维码的纠错级别——这里选择最高H级别
+		// 设置QR二维码的纠错级别——这里选择最低L级别
 		qrParam.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
 		qrParam.put(EncodeHintType.CHARACTER_SET, "utf-8");
 		try {
 			BitMatrix bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height, qrParam);
-			s = toAscii(MatrixToImageWriter.toBufferedImage(bitMatrix));
+			s = toAscii(bitMatrix);
 		} catch (WriterException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -30,12 +28,12 @@ public class QRterminal {
 		return s;
 	}
 
-	public static String toAscii(BufferedImage bim) {
+	public static String toAscii(BitMatrix bitMatrix) {
 		StringBuilder sb = new StringBuilder();
-		for (int rows = 0; rows < bim.getHeight(); rows++) {
-			for (int cols = 0; cols < bim.getWidth(); cols++) {
-				int x = bim.getRGB(rows, cols);
-				if (x == -1) {
+		for (int rows = 0; rows < bitMatrix.getHeight(); rows++) {
+			for (int cols = 0; cols < bitMatrix.getWidth(); cols++) {
+				boolean x = bitMatrix.get(rows, cols);
+				if (!x) {
 					// white
 					sb.append("\033[47m  \033[0m");
 				} else {
